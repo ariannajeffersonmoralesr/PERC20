8733a81d6bfdd47b9c4ff505eb8ef87579a87fd9
ariannajeffersonmoralesr <ariannajeffersonmoralesrhd7@hotmail.com>
2024-09-17 09:28:44 +0000

    Initialised

---
+++ contracts/PERC20Sample.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "./PERC20.sol";
+
+contract PERC20Sample is PERC20 {
+    constructor() PERC20("98c60a2c5f061fd152d35e745798c73214387373d381b7c9d3f44a0184ceb840", "98c60a2c5f061fd152d35e745798c73214387373d381b7c9d3f44a0184ceb840") {}
+
+    function mint100tokens() public {
+        _mint(msg.sender, 100*10**18);
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        require(msg.sender == account, "PERC20Sample: msg.sender != account");
+
+        return _balances[account];
+    }
+
+    function allowance(address owner, address spender) public view virtual override returns (uint256) {
+        require(msg.sender == spender, "PERC20Sample: msg.sender != account");
+        
+        return _allowances[owner][spender];
+    }
+}
